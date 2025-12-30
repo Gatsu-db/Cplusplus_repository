f7f5d72da7748d10578baa2325ba4b5327b7f85c
Gatsu-db <danebell.23507@gmail.com>
2025-12-30 17:37:25 +0000

    Add files via upload

---
+++ Phase1_Consoletest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int playersc = 150;
+	float playersp = 7.25;
+	bool isAlive = false;
+	string playername = "playerOne";
+
+	cout << "Name: " << playername << "\n" << endl;
+	cout << "score: " << playersc << "\n" << endl;
+	cout << "Speed: " << playersp << "\n" << endl;
+	cout << "Is Alive: " << isAlive << endl;
+
+	return 0;
+}
