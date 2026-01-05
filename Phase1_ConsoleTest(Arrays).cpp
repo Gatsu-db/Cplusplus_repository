cfe1d8d010ab02275c84555c1547c284708d2bb6
Gatsu-db <danebell.23507@gmail.com>
2026-01-05 17:30:17 +0000

    Add files via upload

---
+++ Phase1_ConsoleTest(Arrays).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int Ehp[3] = { 9000 , 41, 67 }; //this array has five slots its not dynamic
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "Enemy " << i
+			<< " health: "
+			<< Ehp[i] << endl; // this loop index lets you access each element and avoids copy paste and scales pretty nicely
+		Ehp[i] -= 10;
+	}
+
+	cout << Ehp[0] << " is the First enemy as arrays count from zero" << endl; //first enemy as arrays count from zero
+
+	return 0;
+
+}
